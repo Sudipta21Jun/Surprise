ebaf6a66264e2092e4c8be12ad1c555f8e79be68
Sudipta21Jun <Sudipta.satyam.18@gmail.com>
2024-12-18 20:14:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.getElementById('yesButton').addEventListener('click', function () {
+    alert("Yay! Can't wait to see you on the video call ❤️");
+    // Optionally, redirect to the video call link
+    // window.location.href = "https://zoom.us";
+});
+
+document.getElementById('noButton').addEventListener('click', function () {
+    alert("Haha, nice try! But you know saying no isn’t allowed 😊");
+});
